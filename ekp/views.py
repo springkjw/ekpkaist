7a5e249cec5f841a49a0358b45a4eea6f676d1c6
Kwon Jaewon <springkjw@gmail.com>
2016-03-25 07:24:34 +0900

    book_list

--- ekp/views.py
+++ ekp/views.py
@@ -3,19 +3,22 @@ from django.shortcuts import render
 import json
 import urllib2
 
+import math
+
 def book_list(request, keyword, page):
     page_first = int(page) * 6  - 5
     page_last = int(page) * 6
 
     url = "http://ekp.kaist.ac.kr/apis/getBooks?q=" + keyword + "&prev=" + str(page_first) + "&next=" + str(page_last)
-    print url
+
     query = urllib2.urlopen(url).read()
 
     serialized_obj = json.loads(query)
 
-    count =  serialized_obj['total_doc']/6 + 2
-
-    print serialized_obj['total_doc']
+    if serialized_obj['total_doc']%6 is 0:
+        count = serialized_obj['total_doc']/6 + 1
+    else:
+        count = serialized_obj['total_doc']/6 + 2
 
     template = 'book_list.html'
     context = {
