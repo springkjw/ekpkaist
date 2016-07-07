e528f5126f635ddab13bfbc45e2eb62019491870
Kwon Jaewon <springkjw@gmail.com>
2016-07-08 08:17:06 +0900

    문서화 views_home.py

--- ekp/views_home.py
+++ ekp/views_home.py
@@ -4,22 +4,33 @@ from django.http import HttpResponse, JsonResponse
 import json
 import urllib2
 import urllib
+
+
+# 종합보기 view
 def home(request):
+    # 환자 데이터 30개 가져오기
     url = 'http://kecidev.kaist.ac.kr:50000/cases?limit=30'
     query = urllib2.urlopen(url).read()
 
+    # 환자 데이터 serialize
     serialized_obj = json.loads(query)
 
     if request.is_ajax():
+        # 환자 클릭 시 진단 결과 데이터 가져오기
         if(request.GET.get('id', False)):
+            # case 데이터
             url_data = 'http://kecidev.kaist.ac.kr:50000/cases/' + request.GET['id']
+            # test 데이터
             url_test_data = 'http://kecidev.kaist.ac.kr:50000/cases/' + request.GET['id'] + "/tests"
+            # conclusion 데이터
             url_conclusion_data = 'http://kecidev.kaist.ac.kr:50000/cases/' + request.GET['id'] + "/conclusions"
 
+            # 각 데이터 query
             query_data = urllib2.urlopen(url_data).read()
             query_test_data = urllib2.urlopen(url_test_data).read()
             query_conclusion_data = urllib2.urlopen(url_conclusion_data).read()
 
+            # 각 query json으로 변환
             serialized_obj_data = json.loads(query_data)
             serialized_obj_test_data = json.loads(query_test_data)
             serialized_obj_conclusion_data = json.loads(query_conclusion_data)
@@ -29,28 +40,36 @@ def home(request):
                 "test_data" : serialized_obj_test_data,
                 "conclusion" : serialized_obj_conclusion_data
             }
+            # ajax 결과 반환
             return JsonResponse(context)
+        # conclusion 클릭시 rule과 book 데이터 가져오기
         else:
+            # rule이 여러 개인 경우
             if ',' in request.GET['rule_id']:
+                # 각 rule을 저장
                 serialized_obj_rule_data = []
                 for ids in request.GET['rule_id'].split(','):
                     url_rule_data = 'http://kecidev.kaist.ac.kr:50000/rules/' + ids
                     query_rule_data = urllib2.urlopen(url_rule_data).read()
                     serialized_obj_rule_data.append(json.loads(query_rule_data))
+            # rule이 하나인 경우
             else:
                 url_rule_data = 'http://kecidev.kaist.ac.kr:50000/rules/' + request.GET['rule_id']
 
                 query_rule_data = urllib2.urlopen(url_rule_data).read()
                 serialized_obj_rule_data = json.loads(query_rule_data)
 
+            # book의 keyword 데이터 가져오기
             keyword = urllib.quote_plus(request.GET['text'].encode('utf-8'))
 
+            # book 데이터 가져오기
             try:
                 print keyword
                 url_book_data = 'http://ekp.kaist.ac.kr/apis/getBooks?q=' + keyword + "&prev=1&next=2"
 
                 query_book_data = urllib2.urlopen(url_book_data).read()
                 serialized_obj_book_data = json.loads(query_book_data)
+            # book 데이터가 없는 경우
             except ValueError:
                 serialized_obj_book_data = {
                     'books': [],
@@ -63,6 +82,7 @@ def home(request):
                 "rule" : serialized_obj_rule_data,
                 "book" : serialized_obj_book_data
             }
+            # rule과 book 데이터 반환
             return JsonResponse(context)
 
     else:
@@ -75,15 +95,14 @@ def home(request):
     return render(request, template, context)
 
 
-
-
-
+# 상세보기 view
 def home_patient(request, patient, rule):
     url = 'http://kecidev.kaist.ac.kr:50000/cases?limit=30'
     query = urllib2.urlopen(url).read()
 
     serialized_obj = json.loads(query)
 
+    # 특정 환자에 대한 case, test, conclusion, rule, book 데이터 가져오기
     url_data = 'http://kecidev.kaist.ac.kr:50000/cases/' + patient
     url_test_data = 'http://kecidev.kaist.ac.kr:50000/cases/' + patient + "/tests"
     url_conclusion_data = 'http://kecidev.kaist.ac.kr:50000/cases/' + patient + "/conclusions"
@@ -103,6 +122,7 @@ def home_patient(request, patient, rule):
 
     obj_data = []
     obj_data_ = []
+    # rule이 여러개인 경우
     if len(rule_ids) != 1:
         serialized_obj_rule_data = []
         for i in xrange(0, len(rule_ids)):
@@ -126,7 +146,7 @@ def home_patient(request, patient, rule):
                             'attritube' : i['attribute']
                         }
                         obj_data_.append(data_)
-
+    # rule이 하나인 경우
     else:
         url_rule_data = 'http://kecidev.kaist.ac.kr:50000/rules/' + str(rule_ids[0])
         query_rule_data = urllib2.urlopen(url_rule_data).read()
